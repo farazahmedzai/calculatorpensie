d7c37d4a254092f3a71095e3a22b9b82412aa290
jibranpcccc <jibranpccc@gmail.com>
2026-06-25 20:43:41 +0500

    fix: update 2025 to 2026 in root index.html and ensure calculator redirect survives build

--- build-static.js
+++ build-static.js
@@ -34,6 +34,7 @@ try {
 
     // Create _redirects file for SPA routing only (ads.txt now served as static file)
     const redirectsContent = `/blog/blog/*  /blog/:splat  301!
+/calculator  /  301!
 /robots.txt  /robots.txt  200
 /sitemap.xml  /sitemap.xml  200
 /*          /index.html   200`;
